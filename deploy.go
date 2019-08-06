ec613a0505a3555d814ee358804eabc7a9c96d90
Louis <louisehong4168@gmail.com>
2019-08-06 17:17:34 +0800

    go go go

--- deploy.go
+++ deploy.go
@@ -76,7 +76,7 @@ func main() {
 	// Disable Console Color, you don't need console color when writing the logs to file
 	gin.DisableConsoleColor()
 	// Logging to a file.
-	f, _ := os.Create("gin.log")
+	f, _ := os.Create("/logs/gin.log")
 	gin.DefaultWriter = io.MultiWriter(f)
 	// Use the following code if you need to write the logs to file and console at the same time.
 	// gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
